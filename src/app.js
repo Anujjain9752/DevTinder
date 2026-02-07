24e1161a39955e8931c9f85f8f6cf72de0c4ed54
Anujjain9752 <anuj220jain@gmail.com>
2026-02-07 21:31:43 +0530

    Understanding put patch delelte find etc

--- src/app.js
+++ src/app.js
@@ -6,6 +6,53 @@ const User = require("./models/user");
 
 app.use(express.json()); // to parse incoming JSON data , this is a middleware provided by express to parse the incoming request body as Json
 
+
+// get user by email id
+app.get("/user", async(req,res) => {
+    const email = req.body.emailId
+
+    const user = await User.find({emailId : email})
+
+    if(user.length === 0){
+        res.status(404).json({message: "User not found"})
+    }else{
+        console.log(user)
+        res.status(200).json({message: "User found", user})
+    }
+})
+
+
+// find by ID
+
+app.get("/user/:id", async(req,res) => {
+    const id = req.params.id
+
+    const user = await User.findById(id)
+
+    if(!user){
+        res.status(404).json({message: "User not found"})
+    }else{
+        console.log(user)
+        res.status(200).json({message: "User found", user})
+    }
+})
+
+
+//feed api 
+app.get("/feed", async(req,res) => {
+ 
+   try{
+       const users = await User.find({}) 
+    
+     res.send(users)
+  
+   }catch(err){
+        res.status(500).json({message: "Error fetching feed", error: err.message})
+   }
+
+})
+
+
 app.post("/signup", async (req, res) => {
   // creating a new instance of the user model
   const user = new User(req.body);
@@ -17,6 +64,13 @@ app.post("/signup", async (req, res) => {
     res.status(400).json({ message: "Error adding user", error: err.message });
   }
 });
+
+
+
+//update data of the user
+app.patch("/user", async(req,res)=> {
+    
+})
 
 connectDB()
   .then(() => {
