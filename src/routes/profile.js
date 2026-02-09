ec2e8684e67be3d3b0c5fc1137a1c3e4e8b2919e
Anujjain9752 <anuj220jain@gmail.com>
2026-02-10 00:00:26 +0530

    logout + edit profile api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,29 +1,44 @@
 const express = require("express");
 const profileRouter = express.Router();
-const {userAuth} = require("../middlewares/auth");
+const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+  try {
+    const user = req.user; // we have set the user in the request object in the auth middleware so we can access it here
 
-
-profileRouter.get("/profile", userAuth, async(req,res) => {
-    try{
-        
-        const user = req.user; // we have set the user in the request object in the auth middleware so we can access it here
-
-        if(!user){
-            res.status(404).json({ message: "User not found" });
-        } else {
-            res.status(200).json({ message: "User found", user });
-        }
-      
-        
-
-    } catch(err){
-        res.status(500).json({ message: "Error fetching profile", error: err.message });
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      res.status(200).json({ message: "User found", user });
     }
-})
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error fetching profile", error: err.message });
+  }
+});
 
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid fields in the request body");
+    }
 
+    const loggedInUser = req.user;
 
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key];
+    });
 
+    await loggedInUser.save()
+
+    res.send(`${loggedInUber.firstName}, Profile updated successfully`);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error editing profile", error: err.message });
+  }
+});
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,24 +6,35 @@ const validateSignUpData = (req) => {
 
   if (!firstName || !lastName) {
     throw new Error("All fields are required");
-  }
-  
-  else if (!validator.isEmail(emailId)) {
+  } else if (!validator.isEmail(emailId)) {
     throw new Error("Invalid email address");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong password");
   }
+};
 
-  else if (!validator.isStrongPassword(password)) {
-    throw new Error(
-      "Please enter a strong password"
-    );
-  } 
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
 
-  
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field),
+  ); // this will check if all the fields in the request body are allowed to be edited or not if there is any field which is not allowed to be edited it will return false and we will throw an error
+
+  return isEditAllowed;
 
 
 };
 
-
 module.exports = {
   validateSignUpData,
+  validateEditProfileData,
 };
